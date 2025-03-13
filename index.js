727d960e821193c80fa4caa1aaa7e96ee634fa9e
Jasur <j31548359@gmail.com>
2025-03-13 13:30:04 +0500

    guruhsiz studentlar

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ require("dotenv").config()
 const { connectDB } = require("./config/db");
 connectDB()
 const express = require('express');
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 100,
-    message: "Keyinroq urinib ko'ring"
-});
+// const limiter = rateLimit({
+//     windowMs: 1 * 60 * 1000,
+//     max: 100,
+//     message: "Keyinroq urinib ko'ring"
+// });
 
 app.use(cors());
 app.use(express.json());
-app.use(limiter);
+// app.use(limiter);
 app.use("/", require("./routes/index"))
 
 app.listen(port, () => {
